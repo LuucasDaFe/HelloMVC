3f83aeaed59a2bb4835949550e113dded19b2055
Lucas Abraao <Labraao957@gmail.com>
2023-09-27 23:43:26 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/WelcomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace HelloMVC.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Status(String nome, int id)
+        {
+            ViewData["id"] = id;
+            ViewData["nome"] = nome;
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    return HtmlEncoder.Default.Encode("Index do WelcomeController");
+
+        //}
+
+        //public string Status(string nome, int id)
+        //{
+        //    return $"Olá {nome} e Código é: {id}, seja bem vindo :)";
+        //}
+    }
+}
---
+++ Data/HelloMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HelloMVC.Models;
+
+namespace HelloMVC.Data
+{
+    public class HelloMVCContext : DbContext
+    {
+        public HelloMVCContext (DbContextOptions<HelloMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HelloMVC.Models.User> User { get; set; } = default!;
+    }
+}
